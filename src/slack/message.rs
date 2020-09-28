fa77076122a0bcff7f4dd8394869f8cd7c24449a
Ion Ostafi <ion.ostafi@ellation.com>
2020-09-28 23:05:52 +0300

    Remove unused code

--- src/slack/message.rs
+++ src/slack/message.rs
@@ -2,31 +2,6 @@ use std::collections::BTreeMap;
 
 use crate::git::{RetroCommit, WorkingBranch};
 
-const SUFFIX_EMOJIES: [char; 10] = ['🙌', '👍', '🙏', '🎉', '🚀', '🤘', '👏', '🙌', '👍', '🙏'];
-const START_ROW: &str = r#"
-A reminder on how cool you all are 😎
-A year ago, this same day you've written history 📜
-"#;
-
-pub fn prettify(commits: &[String]) -> String {
-    let mut index = 0usize;
-    let mut commits_count = 0u16;
-    let mut pretty = commits
-        .iter()
-        .fold(String::from(START_ROW), |mut pretty, commit| {
-            pretty.push_str(&format!("☞ {} {} \n", commit, SUFFIX_EMOJIES[index]));
-            index = increase_index(index);
-            commits_count += 1;
-            pretty
-        });
-    pretty.push_str(&format!("{} merged commits in one day", commits_count));
-    if commits_count == 0 {
-        "".into()
-    } else {
-        pretty
-    }
-}
-
 pub fn create_message<C, B>(commits: C, branches: B) -> String
 where
     C: AsRef<[RetroCommit]>,
@@ -62,28 +37,4 @@ where
         message.push_str("```\n");
     }
     message
-}
-
-fn increase_index(i: usize) -> usize {
-    let next = i + 1;
-    if next >= SUFFIX_EMOJIES.len() {
-        0
-    } else {
-        next
-    }
-}
-
-#[cfg(test)]
-mod test {
-    #[test]
-    fn test_0_commits_message_is_empty() {
-        let message = super::prettify(&vec![]);
-        assert!(message.len() == 0);
-    }
-
-    #[test]
-    fn test_increase_max_allowed_index() {
-        let next_index = super::increase_index(super::SUFFIX_EMOJIES.len() - 1);
-        assert_eq!(0, next_index);
-    }
 }
